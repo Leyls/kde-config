d2a10d92c8297d2c45462ccad05169ab91c45b1c
OPa <manpolanic@outlook.com>
2021-09-10 16:22:59 +0800

    chinese font configuration
    download sarasa gothic font first

---
+++ emacs/custom.el
@@ -0,0 +1,67 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#000000" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#eaeaea"))
+ '(beacon-color "dodger blue")
+ '(custom-enabled-themes '(cloud))
+ '(fci-rule-color "#424242")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode 'dark)
+ '(package-selected-packages
+   '(envrc uptimes shfmt dotenv-mode htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company slime cask-mode flycheck-relint cl-libify flycheck-package highlight-quoted macrostep aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode company-nixos-options nixos-options nix-buffer nix-sandbox nixpkgs-fmt nix-mode company-terraform terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust racer rust-mode flycheck-nim nim-mode j-mode dune-format dune merlin-eldoc merlin-company merlin tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode flycheck-elm elm-test-runner elm-mode dhall-mode dante haskell-mode reformatter toml-mode company-anaconda anaconda-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink company-php smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode xref-js2 prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs browse-at-remote whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters beacon mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window company-quickhelp company marginalia consult-flycheck embark-consult affe projectile consult embark orderless vertico flycheck-color-mode-line flycheck ibuffer-vc rg wgrep anzu diff-hl diredfl disable-mouse default-text-scale dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq dired-ranger ssh markdown-preview-mode rainbow-blocks leetcode ac-slime))
+ '(session-use-package t nil (session))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#d54e53")
+     (40 . "#e78c45")
+     (60 . "#e7c547")
+     (80 . "#b9ca4a")
+     (100 . "#70c0b1")
+     (120 . "#7aa6da")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "#e78c45")
+     (200 . "#e7c547")
+     (220 . "#b9ca4a")
+     (240 . "#70c0b1")
+     (260 . "#7aa6da")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "#e78c45")
+     (340 . "#e7c547")
+     (360 . "#b9ca4a")))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-button ((t (:background "gainsboro" :foreground "dim gray" :box (:line-width 2 :style released-button) :family "Fira Code"))))
+ '(diff-hl-dired-change ((t (:inherit diff-hl-change :background "powder blue" :foreground "black"))))
+ '(dired-directory ((t (:background "white smoke" :foreground "dodger blue" :weight bold :family "Menlo"))))
+ '(diredfl-autofile-name ((t (:background "light gray"))))
+ '(diredfl-compressed-file-name ((t (:foreground "slate blue"))))
+ '(diredfl-deletion ((t (:background "tomato" :foreground "gold"))))
+ '(diredfl-dir-heading ((t (:background "light gray" :foreground "dim gray"))))
+ '(diredfl-dir-name ((t (:foreground "dodger blue"))))
+ '(diredfl-dir-priv ((t (:foreground "dodger blue"))))
+ '(diredfl-exec-priv ((t (:foreground "sea green"))))
+ '(diredfl-file-name ((t (:foreground "dark goldenrod"))))
+ '(diredfl-file-suffix ((t (:foreground "forest green"))))
+ '(diredfl-flag-mark ((t (:background "royal blue" :foreground "white smoke"))))
+ '(diredfl-flag-mark-line ((t (:background "pink" :foreground "deep sky blue"))))
+ '(diredfl-no-priv ((t nil)))
+ '(diredfl-number ((t (:foreground "dark goldenrod"))))
+ '(diredfl-other-priv ((t (:foreground "maroon"))))
+ '(diredfl-rare-priv ((t (:background "khaki" :foreground "forest green"))))
+ '(diredfl-read-priv ((t (:background "white smoke" :foreground "light sea green"))))
+ '(diredfl-tagged-autofile-name ((t (:background "dark gray" :foreground "white smoke"))))
+ '(diredfl-write-priv ((t (:foreground "royal blue"))))
+ '(mouse ((t (:background "light gray"))))
+ '(tooltip ((t (:inherit Fira\ Code :background "lightyellow" :foreground "dim gray")))))
